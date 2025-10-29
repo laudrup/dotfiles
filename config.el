bb777c0ac45df8fa2623caecb86c5a1cd2e1716a
Kasper Laudrup <kasper.laudrup@man-es.com>
2025-10-29 12:54:25 +0100

    Added global key shortcuts
    Add some of the global F key shortcuts I'm used to

--- config.el
+++ config.el
@@ -74,3 +74,12 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+(map!
+ "<f8>" #'goto-line
+ )
+
+(after! magit
+  (map!
+   "<f11>" #'magit
+   )
+  )
